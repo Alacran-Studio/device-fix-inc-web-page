23c1b3b025fba63a90c2fa8c5d734c1a190545bc
Jorge Viramontes <>
2021-11-04 12:27:23 -0500

    Create welcome banner div; create index module in styles directory; import styles module & set basic css properties.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,25 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+import * as styles from '../style/index.module.css'
+
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Device Repair Shop</h1>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <h2>COMMUNITY MATTERS</h2>
+
+    <div className={styles.welcomeBannerContainer}>
+      <h1 className={styles.welcomeBannerTitle}>Device Repair Shop</h1>
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={300}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+      <h2>COMMUNITY MATTERS</h2>
+    </div>
+
 
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
