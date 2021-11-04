320a04c5704642e448c66a207f31c13998e45d78
Jorge Viramontes <>
2021-11-04 13:03:17 -0500

    Added template HTML with information of a device repair shop.

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,8 +26,102 @@ const IndexPage = () => (
 
 
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
+      This is an intro paragraph. Look at the company Instagram to really tell a good story.
     </p>
+
+    <div>
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={120}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+      <p>This is the bio of someone. This is more details, and we should probably get these details from somewhere and agree on the copy and what it's going to say. But it will probably be something fancy and regal and simple and nice. Their picture should also be in an oval.</p>
+    </div>
+
+    <div>
+      <p>This is the bio of someone else. This is more details, and we should probably get these details from somewhere and agree on the copy and what it's going to say. But it will probably be something fancy and regal and simple and nice. Their picture should also be in an oval.</p>
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={120}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+    </div>
+
+    <hr />
+
+    <div>
+      <p>
+        P: (312) 965-4899
+        <br />
+        1419 W 18th Street
+        <br />
+        Chicago, IL 60608
+      </p>
+      <h2>Hours of Operation</h2>
+      <p>
+        11AM-6PM Monday thru Friday
+        <br />
+        12-5PM Saturday
+        <br />
+        Closed on Sundays
+      </p>
+    </div>
+
+    <hr />
+
+    <div>
+      <h1>Gallery</h1>
+
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={120}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={120}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={120}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={120}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+    </div>
+
+    <hr />
+
+    <div>
+      <p>Thank you for your support!</p>
+      <p>Make sure to follow us on Instagram <span>-insert Instagram icon here-</span></p>
+    </div>
+
   </Layout>
 )
 
