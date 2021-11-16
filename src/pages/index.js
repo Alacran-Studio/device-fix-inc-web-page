d5a3ab21bcd4cde039b95f87560b3b4f439751ec
Amber Abreu <ambermabreu@gmail.com>
2021-11-16 10:16:51 -0500

    Not too styled border

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,18 @@ const IndexPage = () => (
     <Seo title="Home" />
 
     <div className={styles.welcomeBannerContainer}>
+      <div className={styles.border} style={{ marginTop: "20px" }}>
+        <StaticImage
+          src="../images/regal-border.png"
+          width={100}
+          style={{ transform: "scaleY(-1)" }}
+        />
+        <StaticImage
+          src="../images/regal-border.png"
+          width={100}
+          style={{ transform: "scale(-1, -1)" }}
+        />
+      </div>
       <div className={styles.logo}>
         <StaticImage
           src="../images/device-fix-logo.png"
@@ -19,7 +31,6 @@ const IndexPage = () => (
           placeholder="blurred"
           formats={["auto", "webp", "avif"]}
           alt="Device Fix Inc. Logo"
-          style={{ marginBottom: `1.45rem` }}
         />
       </div>
 
@@ -33,13 +44,27 @@ const IndexPage = () => (
         style={{ marginBottom: `1.45rem` }}
       />
       <h2 className={styles.welcomeBannerSubtitle}>COMMUNITY MATTERS</h2>
+      <div className={styles.border} style={{ marginBottom: `50px` }}>
+        <StaticImage src="../images/regal-border.png" width={100} />
+        <StaticImage
+          src="../images/regal-border.png"
+          style={{ transform: "scaleX(-1)" }}
+          width={100}
+        />
+      </div>
     </div>
 
     <div className={styles.introCopy}>
       <p>
-        Since 2009, we’ve been in the cell phone industry. During this time, we saw the void in our neighborhoods when it came to servicing defective phones, tablets, and game consoles. Insurance companies can have outrageous policies, mandates, and expensive prices.
+        Since 2009, we’ve been in the cell phone industry. During this time, we
+        saw the void in our neighborhoods when it came to servicing defective
+        phones, tablets, and game consoles. Insurance companies can have
+        outrageous policies, mandates, and expensive prices.
       </p>
-      <p>Fast forward a couple of years and we’ve decided to open Device Fix in order to be a resource for our community.</p>
+      <p>
+        Fast forward a couple of years and we’ve decided to open Device Fix in
+        order to be a resource for our community.
+      </p>
       <p>We offer more than cell phone fixing, we are a one stop shop.</p>
     </div>
 
@@ -73,7 +98,7 @@ const IndexPage = () => (
           style={{
             marginBottom: `1.45rem`,
             marginLeft: `auto`,
-            marginRight: `auto`
+            marginRight: `auto`,
           }}
         />
 
@@ -82,7 +107,10 @@ const IndexPage = () => (
             <h2 className={styles.subtitle}>
               Meet Gerardo
               <span className={styles.instagramIcon}>
-                <a href="https://www.instagram.com/titothedancer/" target="blank">
+                <a
+                  href="https://www.instagram.com/titothedancer/"
+                  target="blank"
+                >
                   <StaticImage
                     src="../images/instagram-icon.png"
                     width={24}
@@ -94,9 +122,15 @@ const IndexPage = () => (
               </span>
             </h2>
             <p>
-              His passion comes from teaching youth with coordinated dance. Growing up in the SouthEast of Chicago, his passion for technology started when he was young. His daily drive comes from his lovely family and his newborn son.
+              His passion comes from teaching youth with coordinated dance.
+              Growing up in the SouthEast of Chicago, his passion for technology
+              started when he was young. His daily drive comes from his lovely
+              family and his newborn son.
             </p>
-            <p>His flourishing growth has now made him a proud owner of Device Fix.</p>
+            <p>
+              His flourishing growth has now made him a proud owner of Device
+              Fix.
+            </p>
           </div>
 
           <div>
@@ -115,13 +149,15 @@ const IndexPage = () => (
               </span>
             </h2>
             <p>
-              His mission in business has always been to grow and help others grow in order to be successful in business; it’s a “one hand helps the other” approach. He believes in community and youth resource investment as it has always paid dividends in the success of his businesses. He’s part owner in Device Fix.
+              His mission in business has always been to grow and help others
+              grow in order to be successful in business; it’s a “one hand helps
+              the other” approach. He believes in community and youth resource
+              investment as it has always paid dividends in the success of his
+              businesses. He’s part owner in Device Fix.
             </p>
           </div>
         </div>
       </div>
-
-
     </div>
 
     <hr className={styles.white}></hr>
@@ -130,7 +166,10 @@ const IndexPage = () => (
       <div>
         <h2 className={styles.subtitle}>Contact Us</h2>
         <p>
-          P: <a className={styles.whiteText} href="tel:+1-800-555-5555">(312) 965-4899</a>
+          P:{" "}
+          <a className={styles.whiteText} href="tel:+1-800-555-5555">
+            (312) 965-4899
+          </a>
           <br />
           1419 W 18th Street
           <br />
@@ -155,7 +194,6 @@ const IndexPage = () => (
     <div>
       <h1 className={styles.goldSubsectionTitle}>Gallery</h1>
       <div className={styles.flexContainer}>
-
         <StaticImage
           className={styles.galleryImage}
           src="../images/storefront.JPG"
@@ -243,14 +281,16 @@ const IndexPage = () => (
           formats={["auto", "webp", "avif"]}
           alt="A Gatsby astronaut"
         />
-
       </div>
     </div>
 
     <hr className={styles.gold}></hr>
 
     <div className={styles.centering}>
-      <p>Thank you for your support and we look forward to meeting you and servicing your needs.</p>
+      <p>
+        Thank you for your support and we look forward to meeting you and
+        servicing your needs.
+      </p>
       <p>
         Follow us on Instagram
         <span className={styles.instagramIcon}>
